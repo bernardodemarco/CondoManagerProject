735c66f00f3b0c09831649d300f660bc5249d56a
Manei655 <josevictorvasc@gmail.com>
2022-10-17 20:43:23 -0300

    Criação dos elementos

--- controllers/controlador.py
+++ controllers/controlador.py
@@ -1,3 +1,16 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
 
 class Controlador(ABC):
+    def __init__(self):
+        pass
+
+    
+    @abstractmethod
+    def retornar(self):
+        pass
+
+    @abstractmethod
+    def abre_tela(self):
+        pass
+    
--- controllers/controlador_condominio.py
+++ controllers/controlador_condominio.py
@@ -1,3 +1,11 @@
 from controlador import Controlador
+from controlador_sistema import ControladorSistema
+from models.condominio import Condominio
+
 
 class ControladorCondominio(Controlador):
+    def __init__(self, condominio: Condominio,
+                 controlador_sistema: ControladorSistema):
+        super().__init__()
+        self.__condominio = condominio
+        self.__controlador_sistema = controlador_sistema
