a29eee9bc54de329b7f24aa9fef8b299aa57960b
Manei655 <josevictorvasc@gmail.com>
2022-10-17 21:14:12 -0300

    mudancas rapidas

--- controllers/controlador.py
+++ controllers/controlador.py
@@ -1,15 +1,14 @@
 from abc import ABC, abstractmethod
+from controlador_sistema import ControladorSistema
 
 
 class Controlador(ABC):
     @abstractmethod
-    def __init__(self):
-        pass
+    def __init__(self, controlador_sistema: ControladorSistema):
+        self.__controlador_sistema = controlador_sistema
 
-    
-    @abstractmethod
     def retornar(self):
-        pass
+        self.__controlador_sistema.abre_tela()
 
     @abstractmethod
     def abre_tela(self):
--- controllers/controlador_condominio.py
+++ controllers/controlador_condominio.py
@@ -1,11 +1,19 @@
 from controlador import Controlador
 from controlador_sistema import ControladorSistema
 from models.condominio import Condominio
+from views.tela_condominio import TelaCondominio
 
 
 class ControladorCondominio(Controlador):
-    def __init__(self, condominio: Condominio,
-                 controlador_sistema: ControladorSistema):
-        super().__init__()
-        self.__condominio = condominio
-        self.__controlador_sistema = controlador_sistema
+    def __init__(self, controlador_sistema: ControladorSistema):
+        super().__init__(controlador_sistema)
+        self.__condominios = []
+        self.__tela_condominio = TelaCondominio()
+
+    def pega_condominio_por_num(self, num: int):
+        for condo in self.__condominios:
+            if(condo.num == num):
+                return condo
+        return None
+
+    def incluir_condo(self):
