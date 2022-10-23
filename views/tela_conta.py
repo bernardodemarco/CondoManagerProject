0eb907a15dcb3dd5fc76e6fef267f46c44ff8138
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2022-10-23 18:13:40 -0300

    arrumando tela conta

--- views/tela_conta.py
+++ views/tela_conta.py
@@ -1,5 +1,6 @@
 from views.tela import Tela
 
+
 class TelaConta(Tela):
     def __init__(self) -> None:
         super().__init__()
@@ -12,31 +13,44 @@ class TelaConta(Tela):
         print("        2 - Alterar conta")
         print("        3 - Excluir conta")
         print("        4 - Listar contas")
+        print("        5 - Incluir tipo conta")
+        print("        6 - Alterar tipo conta")
+        print("        7 - Excluir tipo conta")
+        print("        8 - Listar tipo conta")
         print("        0 - Retornar")
         print("<=======<<===========>>=======> \033[0m")
-        return self.checa_opcao(4)
-
-    def pega_dados_tipo(self):
-        print("<=======<<DADOS TIPO DA CONTA>>=======>")
-        tipo = input('Digite o nome do tipo da conta:')
-        return tipo
+        return self.checa_opcao(8)
 
     def pega_dados_contas(self, **kwargs):
-        tipo = self.pega_dados_tipo()
         print("<=======<<DADOS CONTA>>=======>")
+        
         valor = float(input("Digite o valor da conta: "))
-        mes = input('Digite o mês:')
+        mes = input('Digite o mês: ')
         if kwargs['acao'] == 'alteracao':
             id_conta = kwargs['id_conta']
         else:
             id_conta = int(input('Digite um identificador (número inteiro positivo) para a conta: '))
         
-        if (isinstance(tipo, str) and isinstance(valor, float) and
-                valor > 0 and isinstance(mes, str) and
+        if (isinstance(valor, float) and
+                valor >= 0 and isinstance(mes, str) and
                 isinstance(id_conta, int) and id_conta > 0):
-            return {"tipo": tipo, "valor": valor, "mes": mes, 'id': id_conta}
+            return {"valor": valor, "mes": mes, 'id': id_conta}
         else:
             raise ValueError('Valores inválidos, tente novamente!')
+
+    def pega_dados_tipo(self, **kwargs):
+        print("<=======<<DADOS TIPO DA CONTA>>=======>")
+
+        tipo = input('Digite o nome do tipo da conta: ')
+        if kwargs['acao'] == 'alteracao':
+            id_tipo = kwargs['id_tipo']
+        else:
+            id_tipo = int(input('Digite um identificador (número inteiro positivo) para o tipo da conta: '))        
+        
+        if (isinstance(tipo, str) and
+                isinstance(id_tipo, int) and id_tipo > 0):
+            return {'nome_tipo': tipo, 'id': id_tipo}
+        raise ValueError('Valores inválidos, tente novamente!')
 
     def mostra_conta(self, dados):
         print('TIPO DA CONTA:', dados['tipo'])
@@ -45,15 +59,28 @@ class TelaConta(Tela):
         print('ID DA CONTA:', dados['id'])
         print("<=======<<===========>>=======> \033[0m")
 
+    def mostra_tipo_conta(self, dados):
+        print('TIPO DE CONTA:', dados['nome'])
+        print('ID DO TIPO DE CONTA:', dados['id'])
+        print("<=======<<===========>>=======> \033[0m")
+
     def seleciona_conta(self):
         try:
             id_conta = int(input(('SELECIONE A CONTA (digite o identificador): ')))
             if isinstance(id_conta, int) and id_conta > 0:
                 return id_conta
             raise ValueError
-        except:
+        except ValueError:
+            raise ValueError('Valor do id inválido')
+
+    def seleciona_tipo_conta(self):
+        try:
+            id_tipo = int(input(('SELECIONE O TIPO DE CONTA (digite o identificador): ')))
+            if isinstance(id_tipo, int) and id_tipo > 0:
+                return id_tipo
+            raise ValueError
+        except ValueError:
             raise ValueError('Valor do id inválido')
 
     def mostra_mensagem(self, mensagem: str):
         print(mensagem)
-    
